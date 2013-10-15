64bfc1a8d2403a946e4bca46020098d9ee54dac8
albert.xu <albert.xu@virtualu>
2013-10-15 15:35:30 +0800

    use excerpt instead of full article to optimize performance, small fix to card css

--- scripts/src/controllers/post.js
+++ scripts/src/controllers/post.js
@@ -17,12 +17,11 @@ define(function (require) {
     };
 
     var options = {
-      CACHE_SIZE: 15,
-      ROUTINE_INTERVAL: 1000 * 30, // 30 sec
+      CACHE_SIZE: 10, // 15 is too much, even translate3D will get stuck
+      ROUTINE_INTERVAL: 1000 * 10,
     };
 
     var _current_post = 0,
-        _total_posts = 0,
         _since_id = 0;
 
     // FIXME: Message, what is the angular best practice
@@ -47,6 +46,9 @@ define(function (require) {
       var adapted_data = _.map(data, function (d) {
         d.content = d.content.replace(/\n/g, "<br/>");
 
+        d.excerpt = d.content.substring(0, 140);
+        d.is_excerpt_ = true;
+
         d.zindex_ = 100000 - d.id;
         d.pos_ = 'right';
         d.read_ = false;
@@ -58,27 +60,32 @@ define(function (require) {
       return adapted_data;
     }
 
-    // the last card is allowed to swiped left
-    //   to greet the user with the eyes of Ozil
     function nextPost() {
-      if (_current_post < _total_posts) {
+      var total_posts = $scope.posts.length;
+      // the last card is allowed to swiped left
+      //   to greet the user with the eyes of Ozil
+      if (_current_post < total_posts) {
         var cp = $scope.posts[_current_post];
         if (!cp.read_) {
           read(cp.id);
         }
         cp.pos_ = 'left';
+        cp.is_excerpt_ = true;
 
         _current_post += 1;
-        if (_current_post < _total_posts) {
+        if (_current_post < total_posts) {
           $scope.posts[_current_post].pos_ = 'center';
         }
       }
     }
 
     function prevPost() {
+      var total_posts = $scope.posts.length;
       if (_current_post > 0) {
-        if (_current_post < _total_posts) {
-          $scope.posts[_current_post].pos_ = 'right';
+        if (_current_post < total_posts) {
+          var cp = $scope.posts[_current_post];
+          cp.pos_ = 'right';
+          cp.is_excerpt_ = true;
         }
 
         _current_post -= 1;
@@ -107,21 +114,32 @@ define(function (require) {
     }
 
     function routine() {
-      if (_total_posts <= options.CACHE_SIZE) {
+      if ($scope.posts.length <= options.CACHE_SIZE) {
         Msg.loading.begin('fetching new cards');
         fData.fetch("reports/" + _since_id).then(function (data) {
           if (data.length > 0) {
             _since_id = data.slice(-1)[0].id;
 
             $scope.posts = $scope.posts.concat(adapter(data));
-            _total_posts = $scope.posts.length;
+
+            // $scope.cached_posts = $scope.cached_posts.concat(adapter(data));
+
+            // // the 1st time
+            // if ($scope.posts.length === 0) {
+            //   $scope.posts = [$scope.cached_posts[0]];
+            //   if ($scope.cached_posts[1]) {
+            //     $scope.posts.push($scope.cached_posts[1]);
+            //   }
+            //   if ($scope.cached_posts[2]) {
+            //     $scope.posts.push($scope.cached_posts[2]);
+            //   }
+            // }
 
             Msg.loading.end();
           } else {
             Msg.info('no new cards');
           }
-
-        });
+        }, function (reason) { console.log(reason); });
       } else {
         console.log('clearing left cards');
 
@@ -130,14 +148,9 @@ define(function (require) {
         for (var i = 0; i < count; i++) {
           p = $scope.posts[i];
           // cards read but not left will not be cleared
-          if (p === undefined) {
-            var temp = 1;
-          }
-
           if (p.pos_ === 'left' && p.read_) {
             $scope.posts.splice(i, 1);
             _current_post -= 1;
-            _total_posts -= 1;
             count -= 1;
           }
         }
@@ -147,6 +160,7 @@ define(function (require) {
     }
 
     $scope.posts = [];
+    $scope.cached_posts = [];
     routine();
 
     // interactions
@@ -158,14 +172,18 @@ define(function (require) {
       } else if (gesture.direction === 'right') {
         prevPost();
       } else if (gesture.direction === 'up' || gesture.direction === 'down'){
-        if (gesture.deltaX < -10) {
+        if (gesture.deltaX < -15) {
           nextPost();
-        } else if (gesture.deltaX > 10) {
+        } else if (gesture.deltaX > 15) {
           prevPost();
         }
       } else {
         throw 'NotImplementedException';
       }
+    };
+
+    $scope.toggleExcerpt = function (post) {
+      post.is_excerpt_ = !post.is_excerpt_;
     };
   };
 
